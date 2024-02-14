d32f2f7c1913bc002ee88d440bd704f0364d4c4d
SomaTomita <ldh.eeexile@icloud.com>
2024-02-14 23:17:22 +0900

    add editCard

--- src/components/flashcard-list.vue
+++ src/components/flashcard-list.vue
@@ -1,40 +1,67 @@
 <template>
-  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"> <!-- コンテナの最大幅を設定し、両サイドに余白を確保 -->
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <h2 class="text-2xl text-center mb-6">FlashCard List</h2>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-      <div v-for="card in cards" :key="card.id" 
-	  	class="bg-white p-6 rounded-lg shadow-md flex flex-col justify-between h-52" 
-	  	@click="card.showAnswer = !card.showAnswer"
-	  >
+      <div
+        v-for="card in cards"
+        :key="card.id"
+        class="bg-white p-6 rounded-lg shadow-md flex flex-col justify-between h-52 relative"
+        @click="toggleShowAnswer(card)"
+      >
         <div class="mb-4">
-          <p v-if="!card.showAnswer" class="text-lg text-gray-800">{{ card.question }}</p>
+          <p v-if="!card.showAnswer" class="text-lg text-gray-800">
+            {{ card.question }}
+          </p>
           <p v-else class="text-lg text-gray-600">{{ card.answer }}</p>
+        </div>
+        <div class="absolute bottom-2 right-2 flex space-x-2">
+          <button
+            @click.stop="confirmEdit(card)"
+            class="text-xs bg-blue-200 hover:bg-blue-300 text-blue-800 px-2 py-1 rounded"
+          >
+            Edit
+          </button>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-
 <script setup>
-import { ref, onMounted } from 'vue';
-import { db } from '../firebase';
+import { ref, onMounted } from "vue";
+import { db } from "../firebase";
 import { collection, getDocs } from "firebase/firestore";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 
 const cards = ref([]);
 
-const fetchAllCards = async () => {
+onMounted(async () => {
   try {
+    // すべてのカードの全ての情報を取得
     const querySnapshot = await getDocs(collection(db, "cards"));
-    cards.value = querySnapshot.docs.map(doc => ({
-		id: doc.id, ...doc.data(),
-
-      showAnswer: false // 初期状態ではanswerを非表示
+    cards.value = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+      // 初期状態ではanswerを非表示
+      showAnswer: false,
     }));
   } catch (error) {
     console.error("Error fetching cards:", error);
   }
+});
+
+const toggleShowAnswer = (card) => {
+  card.showAnswer = !card.showAnswer;
 };
 
-onMounted(fetchAllCards);
+const confirmEdit = (card) => {
+  const message = `Are you sure to edit this card?
+  \nQuestion: "${card.question}"\nAnswer: "${card.answer}"`;
+
+  if (window.confirm(message)) {
+    router.push({ name: "EditCard", params: { cardId: card.id } });
+  }
+};
 </script>
