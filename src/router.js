65178b690d80734cc53518596383860e10a26db2
SomaTomita <ldh.eeexile@icloud.com>
2024-02-19 22:40:16 +0900

    add beforeeach

--- src/router.js
+++ src/router.js
@@ -7,15 +7,23 @@ import EditCard from './components/edit-card.vue';
 import EmailVerify from './components/email-verify.vue';
 import ResetPassword from './components/reset-password.vue'
 
+import { auth } from "./firebase"
+
 
 const routes = [
 	{ path: '/', name: 'Login', component: Login },
 	{ path: '/signup', name: 'Signup', component: Signup },
-	{ path: '/lists', name: 'FlashcardList', component: FlashcardList },
-	{ path: '/addcard', name: 'AddCard', component: AddCard },
-	{ path: '/edit/:cardId', name: 'EditCard', component: EditCard },
 	{ path: '/emailverify', name: 'EmailVerify', component: EmailVerify },
 	{ path: '/resetpassword', name: 'ResetPassword', component: ResetPassword },
+	{
+		path: '/lists', name: 'FlashcardList', component: FlashcardList, meta: { requiresAuth: true }
+	},
+	{
+		path: '/addcard', name: 'AddCard', component: AddCard, meta: { requiresAuth: true }
+	},
+	{
+		path: '/edit/:cardId', name: 'EditCard', component: EditCard, meta: { requiresAuth: true }
+	},
 ];
 
 
@@ -23,5 +31,13 @@ const router = createRouter({
 	history: createWebHistory(),
 	routes
 });
+
+
+// requiresAuthがあるページはログイン必須
+router.beforeEach((to, from, next) => {
+	const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+	if (requiresAuth && !auth.currentUser) next({ name: 'Login' })
+	else next()
+})
 
 export default router;
